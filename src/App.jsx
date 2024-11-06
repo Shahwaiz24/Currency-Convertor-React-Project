438b61755a59b4316d9d0e90386e8716cb161914
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-07 02:15:23 +0500

    Configured

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+    
+     <div style={{ backgroundImage:"src/asset/wallpaper.jpg"}} className='w-full h-screen bg-no-repeat bg-fixed'></div>
   )
 }
 
